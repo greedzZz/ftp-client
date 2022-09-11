05b24e5b5b07d47deec801d7695f187e0ac2e923
greedzZz <anvariphone2016@gmail.com>
2022-09-11 13:21:19 +0300

    Added JSONToStudentParser

---
+++ src/main/java/JSONToStudentParser.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
+public class JSONToStudentParser {
+    public void parse(Path path, Map<Integer, String> students) {
+        try {
+            List<String> lines = Files.readAllLines(path);
+            if (lines.size() >= 8) {
+                for (int i = 3; i < lines.size() - 3; i += 4) {
+                    String[] idLine = lines.get(i).strip().split(": ");
+                    String[] nameLine = lines.get(i + 1).split(": \"");
+                    students.put(Integer.parseInt(idLine[1].substring(0, idLine[1].length() - 1)),
+                            nameLine[1].substring(0, nameLine[1].length() - 1));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("An I/O error has occurred");
+        }
+    }
+}
