cf7363f3ca31625b6808ba460aa92676e6da5d1c
greedzZz <anvariphone2016@gmail.com>
2022-09-11 12:52:30 +0300

    StudentToJSONParser io -> nio

--- src/main/java/StudentToJSONParser.java
+++ src/main/java/StudentToJSONParser.java
@@ -1,31 +1,31 @@
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map;
 
 public class StudentToJSONParser {
-    public void parse(Map<Integer, String> students, File file) {
-        try (FileOutputStream fos = new FileOutputStream(file, false);
-             BufferedOutputStream bos = new BufferedOutputStream(fos)) {
-            bos.write("{\n".getBytes(StandardCharsets.UTF_8));
-            bos.write("\t\"students\": [".getBytes(StandardCharsets.UTF_8));
+    public void parse(Map<Integer, String> students, Path path) {
+        try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
+            writer.write("{\n");
+            writer.write("\t\"students\": [");
             if (!students.isEmpty()) {
-                bos.write("\n".getBytes(StandardCharsets.UTF_8));
+                writer.write("\n");
                 long serialNumber = 0;
                 long size = students.entrySet().size();
                 for (Map.Entry<Integer, String> student : students.entrySet()) {
                     serialNumber++;
-                    bos.write("\t\t{\n".getBytes(StandardCharsets.UTF_8));
-                    bos.write(("\t\t\t\"id\": " + student.getKey() + ",\n").getBytes(StandardCharsets.UTF_8));
-                    bos.write(("\t\t\t\"name\": \"" + student.getValue() + "\"\n").getBytes(StandardCharsets.UTF_8));
-                    if (serialNumber != size) bos.write("\t\t},\n".getBytes(StandardCharsets.UTF_8));
-                    else bos.write("\t\t}\n\t".getBytes(StandardCharsets.UTF_8));
+                    writer.write("\t\t{\n");
+                    writer.write(("\t\t\t\"id\": " + student.getKey() + ",\n"));
+                    writer.write(("\t\t\t\"name\": \"" + student.getValue() + "\"\n"));
+                    if (serialNumber != size) writer.write("\t\t},\n");
+                    else writer.write("\t\t}\n\t");
                 }
             }
-            bos.write("]\n".getBytes(StandardCharsets.UTF_8));
-            bos.write("}".getBytes(StandardCharsets.UTF_8));
+            writer.write("]\n");
+            writer.write("}");
+            writer.flush();
         } catch (IOException e) {
             System.out.println("No permission to write to the file");
         }
